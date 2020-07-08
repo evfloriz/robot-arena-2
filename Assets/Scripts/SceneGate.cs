b7993a46b12183eb2ac8fbdceec3f8cde74062eb
evfloriz <ev.floriz@gmail.com>
2020-07-07 19:14:35 -0600

    Made scene gate into a prefab
    Changed the scene gate object and script to make a generic prefab, and
    added one more to scene 1 to change to scene 3.

--- Assets/Scripts/SceneGate.cs
+++ Assets/Scripts/SceneGate.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LoadScene2 : MonoBehaviour
-{
+public class SceneGate : MonoBehaviour
+{    
+    public int sceneNum;
+
+    private string sceneName;
+
+    void Start()
+    {
+        sceneName = "Scene " + sceneNum.ToString();
+
+    }
+    
     void OnTriggerEnter2D(Collider2D coll)
     {
         if (coll.gameObject.tag == "Player")
         {
-            SceneManager.LoadScene("Scene 2");
+            SceneManager.LoadScene(sceneName);
         }
-
-        Debug.Log("collided");
     }
 }
