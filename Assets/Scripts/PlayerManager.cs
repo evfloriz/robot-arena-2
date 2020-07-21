1a29607a3405e3aeefc2bf7070fe4839fa841951
evfloriz <ev.floriz@gmail.com>
2020-07-21 15:00:42 -0600

    Updated sprite sheets, added hover feet pickup
    Updated player and extra sprite sheet with hoverfeet animation and
    pickup. Implemented pickup to set property in PlayerManager to true
    when triggered.

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public static PlayerManager instance = null;
+
+    private bool hoverFeet = false;
+    
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+        
+    }
+
+    void test()
+    {
+        Debug.Log("test");
+    }
+
+    public void ObtainHoverFeet()
+    {
+        hoverFeet = true;
+        Debug.Log(hoverFeet);
+    }
+}
